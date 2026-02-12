4bdd71b40238e14adde5575249d813f7a8909b64
MOHAMMED ABUBAKAR SIDDIQ <143837684+Abu999b@users.noreply.github.com>
2026-02-12 12:00:59 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api'|| process.env.REACT_APP_API_URL ;
+const API_URL =  process.env.REACT_APP_API_URL||'http://localhost:5000/api' ;
 
 console.log('🔗 API URL:', API_URL);
 

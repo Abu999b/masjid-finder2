44ba36e2f8ba36598a0a8e3b4bfafdf168d19090
MOHAMMED ABUBAKAR SIDDIQ <143837684+Abu999b@users.noreply.github.com>
2026-02-12 15:10:53 +0530

    Update API_URL to production environment

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL =  process.env.REACT_APP_API_URL||'http://localhost:5000/api' ;
+const API_URL =  'https://masjidfindbnd2-production.up.railway.app/'||process.env.REACT_APP_API_URL||'http://localhost:5000/api' ;
 
 console.log('🔗 API URL:', API_URL);
 
